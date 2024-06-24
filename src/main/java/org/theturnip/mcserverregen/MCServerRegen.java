4ef2941b1a98cc9b9c2a544c73e485c2ec6fb05e
Ryan Steffan <ryansteffan.biz@gmail.com>
2024-06-23 21:52:27 -0500

    Initial Commit

---
+++ src/main/java/org/theturnip/mcserverregen/MCServerRegen.java
@@ -0,0 +1,17 @@
+package org.theturnip.mcserverregen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCServerRegen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
